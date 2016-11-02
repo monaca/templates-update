6712e42a856aacef6f1eccf14c05db91f0424667
Fran Dios <frankdiox@gmail.com>
2016-11-02 17:44:48 +0900

    Add update script and submodules.

---
+++ update.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function updateOnsenui {
+  libPath="www/lib/onsenui";
+  npm install onsenui
+
+  for D in */; do
+    if [ $D != "node_modules/" ]; then
+      echo "${D}${libPath}";
+      (cd $D && git pull)
+      rm -rf "${D}${libPath}";
+      cp -r "node_modules/onsenui" "${D}${libPath}";
+      (cd $D && git add -A && git commit -m "Update onsenui lib.")
+    fi
+  done
+}
+
+function updateAngular {
+  libPath="www/lib/angular";
+  npm install angular
+
+  for D in */; do
+    if [[ $D == *"-angular1-"* ]]; then
+      echo "${D}${libPath}";
+      rm -rf "${D}${libPath}";
+      cp -r "node_modules/angular" "${D}${libPath}";
+      (cd $D && git add -A && git commit -m "Update angular lib.")
+    fi
+  done
+}
+
+function gitPush {
+  for D in */; do
+    if [ $D != "node_modules/" ]; then
+      echo "${D}";
+      (cd $D && git push)
+    fi
+  done
+}
+
+todo=${1-onsenui}
+echo $todo
+
+case "$todo" in
+  onsenui)
+    updateOnsenui
+    ;;
+  angular)
+    updateAngular
+    ;;
+  push)
+    gitPush
+    ;;
+esac
